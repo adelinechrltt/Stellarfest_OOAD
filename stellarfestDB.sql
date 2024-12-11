88c12fc593c9b421c12a3d5a36979b4160a74b5c
Adeline <adeline.chrltt@gmail.com>
2024-12-12 01:30:25 +0700

    Update DB file

--- stellarfestDB.sql
+++ stellarfestDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2024 at 11:41 AM
+-- Generation Time: Dec 11, 2024 at 07:29 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.7
 
@@ -36,14 +36,6 @@ CREATE TABLE `events` (
   `OrganizerID` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `events`
---
-
-INSERT INTO `events` (`EventID`, `Name`, `Date`, `Location`, `Description`, `OrganizerID`) VALUES
-('E0000000', 'Lorem Ipsum', '0000-00-00', 'Lorem Ipsum', 'NULL', '12345678'),
-('E3162053', 'Kpop Concert', '2024-12-19', 'Seoul, South Korea', 'lorem', 'U7494612');
-
 -- --------------------------------------------------------
 
 --
@@ -53,29 +45,9 @@ INSERT INTO `events` (`EventID`, `Name`, `Date`, `Location`, `Description`, `Org
 CREATE TABLE `invitations` (
   `InvID` varchar(8) NOT NULL,
   `EventID` varchar(8) NOT NULL,
-  `UserID` varchar(8) NOT NULL,
   `InvStatus` varchar(20) NOT NULL,
-  `InvRole` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `invitations`
---
-
-INSERT INTO `invitations` (`InvID`, `EventID`, `UserID`, `InvStatus`, `InvRole`) VALUES
-('I6004521', 'E3162053', 'U2381756', 'Accepted', 'Guest'),
-('I6706792', 'E3162053', 'U7337645', 'Accepted', 'Vendor');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `producthandler`
---
-
-CREATE TABLE `producthandler` (
-  `ProdHandID` varchar(8) NOT NULL,
-  `InvID` varchar(8) NOT NULL,
-  `ProdID` varchar(8) NOT NULL
+  `InvRole` varchar(20) NOT NULL,
+  `UserID` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -88,16 +60,8 @@ CREATE TABLE `products` (
   `ProductID` varchar(8) NOT NULL,
   `UserID` varchar(8) NOT NULL,
   `Name` varchar(50) NOT NULL,
-  `Description` varchar(200) NOT NULL
+  `Description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`ProductID`, `UserID`, `Name`, `Description`) VALUES
-('P1234569', '12345678', 'TestProduct', 'Lorem'),
-('P8491917', 'U7337645', 'Test123', 'Lorem ipsum dolor sit amet');
 
 -- --------------------------------------------------------
 
@@ -106,22 +70,21 @@ INSERT INTO `products` (`ProductID`, `UserID`, `Name`, `Description`) VALUES
 --
 
 CREATE TABLE `users` (
-  `UserID` varchar(8) NOT NULL,
-  `Email` varchar(50) NOT NULL,
+  `Email` varchar(100) NOT NULL,
   `Name` varchar(100) NOT NULL,
   `Password` varchar(100) NOT NULL,
-  `Role` varchar(30) NOT NULL
+  `Role` varchar(30) NOT NULL,
+  `UserID` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`UserID`, `Email`, `Name`, `Password`, `Role`) VALUES
-('12345678', 'adeline@gmail.com', 'adelinechrltt', '1234567', 'Admin'),
-('U2381756', 'golda@gmail.com', 'golda', 'golda1234', 'Guest'),
-('U7337645', 'herobrine1234@gmail.com', 'herobrine1234', 'herobrine1234', 'Vendor'),
-('U7494612', 'caelus@gmail.com', 'caelus', '123456', 'Event Organizer');
+INSERT INTO `users` (`Email`, `Name`, `Password`, `Role`, `UserID`) VALUES
+('caelus@gmail.com', 'caelus', '123456', 'Admin', 'U1234567'),
+('shawn@gmail.com', 'Shawnie', '123456', 'Event Organizer', 'U3730035'),
+('adeline@gmail.com', 'adeline', '123456', 'Vendor', 'U4330719');
 
 --
 -- Indexes for dumped tables
@@ -132,7 +95,7 @@ INSERT INTO `users` (`UserID`, `Email`, `Name`, `Password`, `Role`) VALUES
 --
 ALTER TABLE `events`
   ADD PRIMARY KEY (`EventID`),
-  ADD KEY `OrganizerID` (`OrganizerID`);
+  ADD KEY `fk_events_organizerID` (`OrganizerID`);
 
 --
 -- Indexes for table `invitations`
@@ -141,14 +104,6 @@ ALTER TABLE `invitations`
   ADD PRIMARY KEY (`InvID`),
   ADD KEY `EventID` (`EventID`),
   ADD KEY `UserID` (`UserID`);
-
---
--- Indexes for table `producthandler`
---
-ALTER TABLE `producthandler`
-  ADD PRIMARY KEY (`ProdHandID`),
-  ADD KEY `InvID` (`InvID`),
-  ADD KEY `ProdID` (`ProdID`);
 
 --
 -- Indexes for table `products`
@@ -161,7 +116,8 @@ ALTER TABLE `products`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`UserID`);
+  ADD PRIMARY KEY (`UserID`),
+  ADD KEY `UserID` (`UserID`);
 
 --
 -- Constraints for dumped tables
@@ -171,7 +127,7 @@ ALTER TABLE `users`
 -- Constraints for table `events`
 --
 ALTER TABLE `events`
-  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`OrganizerID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_events_organizerID` FOREIGN KEY (`OrganizerID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `invitations`
@@ -181,17 +137,10 @@ ALTER TABLE `invitations`
   ADD CONSTRAINT `invitations_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `producthandler`
---
-ALTER TABLE `producthandler`
-  ADD CONSTRAINT `producthandler_ibfk_1` FOREIGN KEY (`InvID`) REFERENCES `invitations` (`InvID`),
-  ADD CONSTRAINT `producthandler_ibfk_2` FOREIGN KEY (`ProdID`) REFERENCES `products` (`ProductID`);
-
---
 -- Constraints for table `products`
 --
 ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`);
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
